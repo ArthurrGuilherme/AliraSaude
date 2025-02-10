d6f4627f6c39425e809c45881c062fe7eab60db7
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-02-10 06:49:27 -0300

    Add files via upload

--- js/scrollrevel.js
+++ js/scrollrevel.js
@@ -2,6 +2,6 @@ ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('.TextoPrincipal', 
     {    
-        duration: 400, reset: false, distance: '100px' , origin: 'left',
+        duration: 1000, reset: false, distance: '100px' , origin: 'left',
     }
 );
