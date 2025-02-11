5973a6cac35693e48c9ffc9aacff66998d8d3f17
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-02-11 07:20:31 -0300

    Add files via upload

--- js/scrollrevel.js
+++ js/scrollrevel.js
@@ -2,6 +2,16 @@ ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('.TextoPrincipal', 
     {    
-        duration: 1000, reset: false, distance: '100px' , origin: 'left',
+        duration: 2000, reset: false, distance: '100px' , origin: 'left',
+    }
+);
+ScrollReveal().reveal('#CardRoxo', 
+    {    
+        duration: 1500, reset: true, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('#CardAmarelo', 
+    {    
+        duration: 1500, reset: true, distance: '100px' , origin: 'top',
     }
 );
