600a9d9e0aa4e2fe1a95ac3e53456088eb5c9c84
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-02-26 10:11:54 -0300

    Add files via upload

--- js/mapa.js
+++ js/mapa.js
@@ -9,3 +9,17 @@ document.querySelector("#Planos").addEventListener("click", function (event) {
 
     console.log("go to up")
 })
+document.querySelector("#btnplano").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollto("#planobtn")
+
+    console.log("go to up")
+})
+document.querySelector("#Formulario").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollto("#containerformulario")
+
+    console.log("go to up")
+})
